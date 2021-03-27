58f77c8af077df6292672ac367e00e0ac998cc69
Alena Kulishkina <alenakuliskina@gmail.com>
2021-03-27 19:00:12 +0600

    added a method for finding the sum of digits in a number

--- Test/src/main/Main.java
+++ Test/src/main/Main.java
@@ -1,7 +1,23 @@
 package main;
 
 public class Main {
+    /**
+        Метод считает сумму цифр в целом числе.
+        Вход: целое число num.
+        Выход: сумма цифр числа num.
+        */
+    public static int sumOfDigits(int num){
+        int sum = 0;
+        num = Math.abs(num);
+        while(num != 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
 
     public static void main(String[] args) {
+        int num = -123045;
+        System.out.println(sumOfDigits(num));
     }
 }
