e76a4260721997d03a9d12c40583daa2cb42f438
Alena Kulishkina <alenakuliskina@gmail.com>
2021-03-27 22:48:32 +0600

    added a check for the dimension of the input array

--- Test/src/main/java/Main.java
+++ Test/src/main/java/Main.java
@@ -1,5 +1,3 @@
-package main;
-
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -25,7 +23,19 @@ public class Main {
        Вход: массив вещественных чисел.
        Выход: лист из трех наибольших вещественных чисел.
        */
-    static List<Double> three_highest(double[] numbers) {
+    static List<Double> threeHighest(double[] numbers) {
+        if (numbers == null){
+            return null;
+        }
+        if (numbers.length < 3) {
+            if (numbers.length == 2) {
+                return Arrays.asList(Math.min(numbers[0], numbers[1]), Math.max(numbers[0], numbers[1]), null);
+            }
+            if (numbers.length == 1) {
+                return Arrays.asList(numbers[0], null, null);
+            }
+            return Arrays.asList(null, null, null);
+        }
         LinkedList<Double> maxes = new LinkedList<>(Arrays.asList(Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE));
         int index = 0;
         int counter = 0;
@@ -47,11 +57,5 @@ public class Main {
             index = 0;
             }
         return maxes;
-    }
-
-    public static void main(String[] args) {
-        int num = -123045;
-        System.out.println(sumOfDigits(num));
-        System.out.println(three_highest(new double[]{Double.MIN_VALUE, 1.2, 0.0, -9.9, 45.8, 46.3, 24.2}));
     }
 }
